af27c33670474018c7fd23aad6acf771ce07f226
njine <brikab@gmail.com>
2023-12-21 05:33:27 +0300

    airo

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for AirBnB clone"""
+"""This module defines a class to manage file storage for AirBnB clone."""
+
 import json
 import shlex
 from models.base_model import BaseModel
@@ -12,38 +13,47 @@ from models.review import Review
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """This class manages storage of hbnb models in JSON format."""
 
+    """
+    This class serializes instances to a JSON file and
+    deserializes JSON file to instances
+    Attributes:
+        __file_path: path to the JSON file
+        __objects: objects will be stored
+    """
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage.
-        
-        Args:
-            cls (class): The class type to filter the objects. Defaults to None.
+        """Return a dictionary of models currently in storage.
 
-        Returns:
-            dict: A dictionary containing objects filtered by class if cls is provided,
-                  otherwise, returns all objects.
+        Return:
+            returns a dictionary of __object
         """
-        filtered_objects = {}
+        dic = {}
         if cls:
-            for key, obj in self.__objects.items():
-                if isinstance(obj, cls):
-                    filtered_objects[key] = obj
-            return filtered_objects
+            for key, value in self.__objects.items():
+                partition = key.replace('.', ' ')
+                partition = shlex.split(partition)
+                if partition[0] == cls.__name__:
+                    dic[key] = value
+            return dic
         else:
             return self.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """Add new object to storage dictionary."""
+        """sets __object to given obj
+        Args:
+            obj: given object
+        """
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             self.__objects[key] = obj
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """Save storage dictionary to file."""
         my_dict = {}
         for key, value in self.__objects.items():
             my_dict[key] = value.to_dict()
@@ -51,27 +61,22 @@ class FileStorage:
             json.dump(my_dict, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Load storage dictionary from file."""
         try:
             with open(self.__file_path, 'r', encoding="UTF-8") as f:
-                temp = json.load(f)
-                for key, val in temp.items():
+                my_dict = json.load(f)
+                for key, val in my_dict.items():
                     value = eval(val["__class__"])(**val)
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """Deletes the given object from storage.
-
-        Args:
-            obj (BaseModel): The object to be deleted. Defaults to None, in which case
-                             the method does nothing.
-        """
+        """ Delete an existing element."""
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
 
     def close(self):
-        """Calls reload()"""
+        """ Call reload()."""
         self.reload()
