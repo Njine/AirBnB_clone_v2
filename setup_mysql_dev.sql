545c13dc62945c39461ee1d1677b4135a315aaee
njine <brikab@gmail.com>
2023-12-24 05:20:09 +0300

    supha

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,15 @@
--- Prepares a MySQL server for the project.
+-- Creating hbnb_dev_db database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Creating or updating hbnb_dev user and setting password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
+
+-- Using the hbnb_dev_db database
+USE hbnb_dev_db;
+
+-- Granting privileges to hbnb_dev user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flushing privileges
+FLUSH PRIVILEGES;
