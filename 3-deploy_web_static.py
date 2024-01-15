beeccc0e2264198aeb7a7e012ffacc30d143cb52
njine <brikab@gmail.com>
2024-01-15 03:31:50 +0300

    deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,43 +1,56 @@
-from fabric.api import put, run, env
-from os.path import exists
+#!/usr/bin/python3
+"""Create and distribute an archive to web servers"""
+import os.path
+import time
+from fabric.api import local, put, run, env
 
 env.hosts = ['100.25.130.218', '34.239.253.248']
-env.user = 'ubuntu'
+
+
+def create_archive():
+    """Generate a tgz archive from web_static folder"""
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(time.strftime("%Y%m%d%H%M%S")))
+        return "versions/web_static_{}.tgz".format(
+            time.strftime("%Y%m%d%H%M%S")
+        )
+    except Exception as e:
+        print(f"Error creating archive: {e}")
+        return None
 
 
 def do_deploy(archive_path):
-    """Distribute an archive to the web servers."""
-    if not exists(archive_path):
+    """Distribute an archive to web servers"""
+    if not os.path.isfile(archive_path):
         return False
 
     try:
-        file_name = archive_path.split("/")[-1]
-        no_extension = file_name.split(".")[0]
-        path = "/data/web_static/releases/"
-
-        # Upload the archive
-        put(archive_path, '/tmp/')
-
-        # Create release directory
-        run('mkdir -p {}{}/'.format(path, no_extension))
-
-        # Extract the archive
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_extension))
-
-        # Remove the uploaded archive
-        run('rm /tmp/{}'.format(file_name))
-
-        # Move contents to the release directory
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_extension))
-
-        # Remove the web_static directory
-        run('rm -rf {}{}/web_static'.format(path, no_extension))
-
-        # Update the symbolic link
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_extension))
-
+        file_name = os.path.basename(archive_path)
+        folder = "/data/web_static/releases/{}".format(
+            file_name.split(".")[0]
+        )
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {}/web_static/* {}/".format(folder, folder))
+        run("rm -rf {}/web_static".format(folder))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder))
+        print("Deployment done")
         return True
     except Exception as e:
-        print(e)
+        print(f"Error deploying archive: {e}")
+        return False
+
+
+def deploy():
+    """Create and distribute an archive to web servers"""
+    try:
+        path = create_archive()
+        return do_deploy(path)
+    except Exception as e:
+        print(f"Deployment failed: {e}")
         return False
